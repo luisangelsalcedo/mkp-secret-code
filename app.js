b57bcce43e86dfb38428d2ee12cbe0b2efd6fc47
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2022-02-13 00:48:45 -0500

    add h1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("HcUy6Re2LLBRtj");
+  res.send("<h1>HcUy6Re2LLBRtj</h1>");
 });
 
 app.listen("3000", () => {
